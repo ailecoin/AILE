095b963bf63aa15649405ad9c88fb0b7224f28e0
Aile Coin <ailecoin.master@gmail.com>
2019-09-23 06:08:12 +0900

    Port modify

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -1086,7 +1086,7 @@ std::string HelpExampleRpc(string methodname, string args)
 {
     return "> curl --user myusername --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", "
            "\"method\": \"" +
-           methodname + "\", \"params\": [" + args + "] }' -H 'content-type: text/plain;' http://127.0.0.1:39872/\n";
+           methodname + "\", \"params\": [" + args + "] }' -H 'content-type: text/plain;' http://127.0.0.1:59872/\n";
 }
 
 void RPCRegisterTimerInterface(RPCTimerInterface *iface)

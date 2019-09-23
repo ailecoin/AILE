57dd697cc947344b3b49b93e22359edd85aee07b
ailecoin <ailecoin.master@gmail.com>
2019-09-23 23:35:54 +0900

    Debug code added

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -148,10 +148,10 @@ public:
 
         hashGenesisBlock               = genesis.GetHash();
 		
-		printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
-		printf("genesis.hashMerkleRoot = %s\n", genesis.BuildMerkleTree().ToString().c_str());
+        printf("genesis.hashMerkleRoot = %s\n", genesis.BuildMerkleTree().ToString().c_str());
+		    printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
         assert(hashGenesisBlock        == uint256("0xc7f4deaf2e9acb339facb15b9128db7c385125a09ca17cee9262841a0866495b"));
-        assert(genesis.hashMerkleRoot  == uint256("0x720db66236cbc0145824aa7a04758efa1a126da1ed4d8e2ce3d48d2ec7927026"));
+        assert(genesis.hashMerkleRoot  == uint256("0x286583d06087dfcc80034ab00c3771dcfba55766918817e10f6d85685f9acdb8"));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,  48);	//48 L https://en.bitcoin.it/wiki/List_of_address_prefixes
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,  63);

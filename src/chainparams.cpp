a933ee3ff455eddaa1e93e210639c7cf4d5763a1
ailecoin <ailecoin.master@gmail.com>
2019-09-26 04:58:15 +0900

    Genesis block generate code

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -55,23 +55,23 @@ static void convertSeed6(std::vector<CAddress>& vSeedsOut, const SeedSpec6* data
 // + Contains no strange transactions
 static Checkpoints::MapCheckpoints mapCheckpoints =
     boost::assign::map_list_of
-    (     0, uint256("0x0000022334a1cd7f9cfd9aeefb0563ebd585d6206d1dffde78cd6db24967e402"))
-    (    20, uint256("0x000006d806dc742f88c22774e5e983f3116d9b3bd3386335da27f7e1982b5c22"))
-    (  4740, uint256("0x94687b000606babc37a1e795e213dcb7ced9752cf07670cd066191b88a9004ad"))
-    (  5535, uint256("0x3cfa27266fe9382fdb58231ed440832fac9df3cb5bd491f364aaa1fe2895c9ab"));
+    (     0, uint256("0x000000e0081675b49be9aefa56d9bc8424c1f774b03f51ef84df74908dfe2697"));
+    //(    20, uint256("0x000006d806dc742f88c22774e5e983f3116d9b3bd3386335da27f7e1982b5c22"))
+    //(  4740, uint256("0x94687b000606babc37a1e795e213dcb7ced9752cf07670cd066191b88a9004ad"))
+    //(  5535, uint256("0x3cfa27266fe9382fdb58231ed440832fac9df3cb5bd491f364aaa1fe2895c9ab"));
 
 
 
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
-    1562683961, // * UNIX timestamp of last checkpoint block
-    10788,      // * total number of transactions between genesis and last checkpoint (the tx=... number in the SetBestChain debug.log lines)
+    1562155595,//1562683961, // * UNIX timestamp of last checkpoint block //1569293166
+    1,//10788,      // * total number of transactions between genesis and last checkpoint (the tx=... number in the SetBestChain debug.log lines)
 																			
-    1440        // * estimated number of transactions per day after checkpoint
+    1//1440        // * estimated number of transactions per day after checkpoint
 };
 
 static Checkpoints::MapCheckpoints mapCheckpointsTestnet =
-    boost::assign::map_list_of(0, uint256("0x0000022334a1cd7f9cfd9aeefb0563ebd585d6206d1dffde78cd6db24967e402"));
+    boost::assign::map_list_of(0, uint256("0x000000e0081675b49be9aefa56d9bc8424c1f774b03f51ef84df74908dfe2697"));
 
 static const Checkpoints::CCheckpointData dataTestnet = {
     &mapCheckpointsTestnet,
@@ -81,7 +81,7 @@ static const Checkpoints::CCheckpointData dataTestnet = {
 };
 
 static Checkpoints::MapCheckpoints mapCheckpointsRegtest =
-    boost::assign::map_list_of(0, uint256("0x0000022334a1cd7f9cfd9aeefb0563ebd585d6206d1dffde78cd6db24967e402"));
+    boost::assign::map_list_of(0, uint256("0x000000e0081675b49be9aefa56d9bc8424c1f774b03f51ef84df74908dfe2697"));
 static const Checkpoints::CCheckpointData dataRegtest = {
     &mapCheckpointsRegtest,
     1562155595, // * UNIX timestamp of last checkpoint block
@@ -131,7 +131,7 @@ public:
         nLastPOWBlock                  = 300;
         nModifierUpdateBlock           = 1;
 
-        const char* pszTimestamp       = "Ailecoin by TERRA MILESTONE";
+        const char* pszTimestamp       = "AileCoin by TERRA MILESTONE 2019";
         CMutableTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
@@ -142,18 +142,52 @@ public:
         genesis.hashPrevBlock          = 0;
         genesis.hashMerkleRoot         = genesis.BuildMerkleTree();
         genesis.nVersion               = 1;
-        genesis.nTime                  = 1568764800; //09/18/2019 @ 12:00am (UTC)
+        genesis.nTime                  = 1562155595;
         genesis.nBits                  = 504365040;
-        genesis.nNonce                 = 3295626318;
+        genesis.nNonce                 = 3083325;
+        //genesis.nTime                  = 1568764800; //09/18/2019 @ 12:00am (UTC)
+        //genesis.nBits                  = 504365040;
+        //genesis.nNonce                 = 89;
 
         hashGenesisBlock               = genesis.GetHash();
-		
-		    //printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
-        //printf("genesis.hashMerkleRoot = %s\n", genesis.BuildMerkleTree().ToString().c_str());
-        assert(hashGenesisBlock        == uint256("0xc7f4deaf2e9acb339facb15b9128db7c385125a09ca17cee9262841a0866495b"));
-        assert(genesis.hashMerkleRoot  == uint256("0x286583d06087dfcc80034ab00c3771dcfba55766918817e10f6d85685f9acdb8"));
-
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,  48);	//48 L https://en.bitcoin.it/wiki/List_of_address_prefixes
+        
+        // For Genesis block parameters find
+        if(genesis.GetHash() != uint256("0x"))
+        {
+        printf("MSearching for genesis block...\n");
+        uint256 hashTarget;
+        hashTarget.SetCompact(genesis.nBits);
+        while(uint256(genesis.GetHash()) > uint256(hashTarget))
+        {
+            ++genesis.nNonce;
+            if (genesis.nNonce == 0)
+            {
+                printf("Mainnet NONCE WRAPPED, incrementing time");
+                std::cout << std::string("Mainnet NONCE WRAPPED, incrementing time:\n");
+                ++genesis.nTime;
+            }
+            if (genesis.nNonce % 10000 == 0)
+            {
+               printf("Mainnet: nonce %08u: hash = %s \n", genesis.nNonce, genesis.GetHash().ToString().c_str());
+            }
+        }
+        printf("Mainnet block.nTime = %u \n", genesis.nTime);
+        printf("Mainnet block.nNonce = %u \n", genesis.nNonce);
+        printf("Mainnet block.hashMerkleRoot: %s\n", genesis.hashMerkleRoot.ToString().c_str());
+        printf("Mainnet block.GetHash = %s\n", genesis.GetHash().ToString().c_str());
+        }
+        assert(hashGenesisBlock        == uint256("0x000000e0081675b49be9aefa56d9bc8424c1f774b03f51ef84df74908dfe2697"));
+        assert(genesis.hashMerkleRoot  == uint256("0xa9a10c28b0548005434fa3c3b6364704967f54db96f948b8cbe9dd850a9ba6da"));
+        
+        /*		
+		    printf("Main network genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
+        printf("Main network genesis.hashMerkleRoot = %s\n", genesis.BuildMerkleTree().ToString().c_str());
+        assert(hashGenesisBlock        == uint256("0xcc7793dddbaa720f9f5a336dcc1d513f49637d8f5f05828bd764ecf50d86d0e5"));
+        assert(genesis.hashMerkleRoot  == uint256("0x7db4260f71df184bc721267b3c28e0be108c28befe12b6a9eb630063e230681b"));
+        //assert(hashGenesisBlock        == uint256("0x84942c354ffc73b008e57bd36f7a1dab54a73cf987505f34a6d02f9bb56d8719"));
+        //assert(genesis.hashMerkleRoot  == uint256("0x25a3770c7d20a451b6e3f7553fc2ed579da36face51a42d997d6ad170923b33c"));
+        */
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,  23);	//23 A https://en.bitcoin.it/wiki/List_of_address_prefixes
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,  63);
         base58Prefixes[SECRET_KEY]     = std::vector<unsigned char>(1, 193);
 
@@ -166,17 +200,17 @@ public:
 
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 
-        vSeeds.push_back(CDNSSeedData("dnsseed.ailecoin.com", "dnsseed.ailecoin.com"));
-		vSeeds.push_back(CDNSSeedData("dnsseed.ailecoin.net", "dnsseed.ailecoin.net"));
-        vSeeds.push_back(CDNSSeedData("dnsseed.ailecoin.io", "dnsseed.ailecoin.io"));
-		vSeeds.push_back(CDNSSeedData("dnsseed.ailecoin.kr", "dnsseed.ailecoin.kr"));
-		vSeeds.push_back(CDNSSeedData("dnsseed.ailecoin.co.kr", "dnsseed.ailecoin.co.kr"));
+        //vSeeds.push_back(CDNSSeedData("dnsseed.ailecoin.com", "dnsseed.ailecoin.com"));
+        //vSeeds.push_back(CDNSSeedData("dnsseed.ailecoin.net", "dnsseed.ailecoin.net"));
+        //vSeeds.push_back(CDNSSeedData("dnsseed.ailecoin.io", "dnsseed.ailecoin.io"));
+        //vSeeds.push_back(CDNSSeedData("dnsseed.ailecoin.kr", "dnsseed.ailecoin.kr"));
+        //vSeeds.push_back(CDNSSeedData("dnsseed.ailecoin.co.kr", "dnsseed.ailecoin.co.kr"));
 
-        fMiningRequiresPeers           = true;
+        fMiningRequiresPeers           = false;  //Mod. Or = true;
         fAllowMinDifficultyBlocks      = false;
         fDefaultConsistencyChecks      = false;
         fRequireStandard               = true;
-        fMineBlocksOnDemand            = false;
+        fMineBlocksOnDemand            = true;  //Mod. Or = false;
         fSkipProofOfWorkCheck          = false;
         fTestnetToBeDeprecatedFieldRPC = false;
         fHeadersFirstSyncingActive     = false;
@@ -235,12 +269,14 @@ public:
         nMaxMoneyOut                   = 30000000 * COIN;
 
         //! Modify the testnet genesis block so the timestamp is valid for a later start.
-        genesis.nTime                  = 1568764800;
-        genesis.nNonce                 = 3295626318;
+        genesis.nTime                  = 1562155595;
+        genesis.nNonce                 = 3047514;
 
         hashGenesisBlock               = genesis.GetHash();
-        assert(hashGenesisBlock        == uint256("0xc7f4deaf2e9acb339facb15b9128db7c385125a09ca17cee9262841a0866495b"));
-        assert(genesis.hashMerkleRoot  == uint256("0x286583d06087dfcc80034ab00c3771dcfba55766918817e10f6d85685f9acdb8"));
+		    printf("Testnet genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
+        printf("Testnet genesis.hashMerkleRoot = %s\n", genesis.BuildMerkleTree().ToString().c_str());
+        assert(hashGenesisBlock        == uint256("0xdbeebdd5527e9ff9ff95aaf0ca09a88721d3f9fbe8b79781d1125fdc542a1e23"));
+        assert(genesis.hashMerkleRoot  == uint256("0xa9a10c28b0548005434fa3c3b6364704967f54db96f948b8cbe9dd850a9ba6da"));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,  83);	// 83 a
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 125);
@@ -300,13 +336,16 @@ public:
         nTargetTimespan = 24 * 60 * 60; // AileCoin: 1 day
         nTargetSpacing = 1 * 60;        // AileCoin: 1 minutes
         bnProofOfWorkLimit = ~uint256(0) >> 1;
-        genesis.nTime = 1516926684;
-        genesis.nBits = 0x207fffff;
-        genesis.nNonce = 20542300;
+        genesis.nTime = 1562155595;
+        genesis.nBits = 504365040;
+        genesis.nNonce = 3083325;
+        //genesis.nTime = 1568764800;
+        //genesis.nBits = 504365040;
+        //genesis.nNonce = 89;
 
         hashGenesisBlock = genesis.GetHash();
         nDefaultPort = 59876;
-       // assert(hashGenesisBlock == uint256("0x0000022334a1cd7f9cfd9aeefb0563ebd585d6206d1dffde78cd6db24967e402"));
+       // assert(hashGenesisBlock == uint256("0x000000e0081675b49be9aefa56d9bc8424c1f774b03f51ef84df74908dfe2697"));
 
         vFixedSeeds.clear(); //! Regtest mode doesn't have any fixed seeds.
         vSeeds.clear();      //! Regtest mode doesn't have any DNS seeds.
